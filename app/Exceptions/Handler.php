dc18a0a308e5278ae178a97a0778c00c8c7404ce
sarawutwn <52755966+sarawutwn@users.noreply.github.com>
2021-01-21 11:29:40 +0700

    เพิ่ม response เวลาไม่มี token

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -36,5 +37,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if (!$request->expectsJson()){
+            return response()->json(['status' => 403, 'message' => 'Your accesstoken not define!'], 403);
+        }
     }
 }
